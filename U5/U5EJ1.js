1e8b2bd2a2620ab52fabc4e53a9f681ed8ef2613
j2 <j2@mac-book-pro-joan.local>
2025-12-29 14:21:36 +0100

    finished

--- U5/U5EJ1.js
+++ U5/U5EJ1.js
@@ -61,8 +61,6 @@ const classroomStudents = [
  
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
 
-
-
 const letterGrade = student => (student.averageGrade >= 9.0) ?  'A' : (student.averageGrade >= 8.0) ?  'B' : (student.averageGrade >= 3.0) ? 'C' : "D"
 
 const result = classroomStudents.map(letterGrade);
